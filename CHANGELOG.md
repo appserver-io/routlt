3190374f960ca203a542b87d48eeae25f43b5b19
Tim Wagner <t.wagner@techdivision.com>
2014-07-24 16:46:58 +0200

    Switch to new appserver-io/build build- and deployment environment

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,3 +8,4 @@ Features
 
 * Refactoring ANT PHPUnit execution process
 * Composer integration by optimizing folder structure (move bootstrap.php + phpunit.xml.dist => phpunit.xml)
+* Switch to new appserver-io/build build- and deployment environment
--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,24 @@
 <?php
 
+/**
+ * boostrap.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @category  Library
+ * @package   Routlt
+ * @author    Tim Wagner <tw@techdivision.com>
+ * @copyright 2014 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://github.com/appserver-io/routlt
+ * @link      http://www.appserver.io
+ */
+
 $loader = require 'vendor/autoload.php';
-$loader->add('AppserverIo\\Routlt\\', 'src');
+$loader->add('AppserverIo\\Routlt', 'src');
