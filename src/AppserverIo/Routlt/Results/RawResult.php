ed13cc3df1984fb47a14ed9bde6fc91a14f979b3
wagnert <wagner_tim78@hotmail.com>
2016-08-16 22:27:40 +0200

    Remove invalid deprecated annotation from RawResult implementation

--- src/AppserverIo/Routlt/Results/RawResult.php
+++ src/AppserverIo/Routlt/Results/RawResult.php
@@ -40,7 +40,6 @@ use AppserverIo\Psr\Servlet\ServletResponseInterface;
  * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link       http://github.com/appserver-io/routlt
  * @link       http://www.appserver.io
- * @deprecated Since 2.0.0-alpha5, use AppserverIo\Routlt\Results\RawResult instead
  */
 class RawResult implements ResultInterface, ActionAware
 {
